bf26797b2418c9735cb4716ba6accb31d5699cd6
Matthieu Dorier <mdorier@anl.gov>
2022-07-05 15:39:53 +0000

    added run-static.sh for theta

---
+++ theta/amr-wind/input/laptop_scale.damBreak.i
@@ -0,0 +1,81 @@
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#            SIMULATION STOP            #
+#.......................................#
+time.stop_time               =   10     # Max (simulated) time to evolve
+time.max_step                =   20     # Max number of time steps
+time.init_shrink = 0.001
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+# TIME STEP COMPUTATION #
+#.......................................#
+time.fixed_dt         =   0.001        # Use this constant dt if > 0
+time.cfl              =   0.8           # CFL factor
+time.initial_dt = 0.001
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+# INPUT AND OUTPUT #
+#.......................................#
+io.KE_int = 0
+io.line_plot_int = 5
+#io.restart_file = chk00300
+time.plot_interval            =  5       # Steps between plot files
+time.checkpoint_interval      =  300     # Steps between checkpoint files
+time.checkpoint_start         =  300
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+# PHYSICS #
+#.......................................#
+incflo.use_godunov = 1
+incflo.godunov_type="weno"
+transport.model = TwoPhaseTransport
+transport.laminar_prandtl = 0.7
+transport.turbulent_prandtl = 0.3333
+turbulence.model = Laminar
+
+incflo.physics = MultiPhase DamBreak
+MultiPhase.density_fluid1=1000.
+MultiPhase.density_fluid2=1.0
+MultiPhase.verbose = 1
+DamBreak.height=0.0571
+DamBreak.width=0.0571
+ICNS.source_terms = GravityForcing
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+# ADAPTIVE MESH REFINEMENT #
+#.......................................#
+amr.n_cell              = 80 16 24      # Grid cells at coarsest AMRlevel
+amr.max_level           = 1             # Max AMR level in hierarchy
+amr.max_grid_size      = 64
+amr.blocking_factor     = 8
+time.regrid_interval =  5
+tagging.labels = TO1
+tagging.TO1.type = InterfaceThicknessRefinement
+tagging.labels = grad
+tagging.grad.type = GradientMagRefinement
+tagging.grad.field_name = density
+tagging.grad.values = 10 10 10 10 10
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+# GEOMETRY #
+#.......................................#
+geometry.prob_lo = 0 0. 0. # Lo corner coordinates
+geometry.prob_hi = 0.2855 0.0571 0.08565 # Hi corner coordinates
+geometry.is_periodic = 0 1 0 # Periodicity x y z (0/1)
+
+xlo.type = "slip_wall"
+xhi.type = "slip_wall"
+zlo.type = "slip_wall"
+zhi.type = "slip_wall"
+
+#incflo.post_processing = ascent
+#ascent.type = Ascent
+#ascent.fields = velocity
+#ascent.output_frequency = 5
+
+incflo.post_processing = colza
+colza.type = Colza
+colza.fields = velocity
+colza.protocol = "ofi+gni"
+colza.provider_id = 0
+colza.ssg_file = "colza.ssg"
+colza.pipeline_name = "colza_ascent_pipeline"
---
+++ theta/amr-wind/run-static.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#COBALT -A radix-io
+#COBALT -t 0:10:00
+#COBALT --mode script
+#COBALT -n 4
+#COBALT -q debug-flat-quad
+#COBALT --attrs filesystems=home,grand,eagle,theta-fs0
+
+export MPICH_GNI_NDREG_ENTRIES=1024
+
+function print_log() {
+    MSG=$1
+    NOW=`date +"%Y-%m-%d %T.%N"`
+    echo "[$NOW] $MSG" | tee -a $MAIN_LOG
+}
+
+HERE=`dirname $0`
+HERE=`realpath $HERE`
+
+NUM_HOSTS=$COBALT_JOBSIZE
+NUM_AMRWIND_HOSTS=${1:-2}
+NUM_COLZA_HOSTS=${2:-2}
+
+if (( $NUM_AMRWIND_HOSTS + $NUM_COLZA_HOSTS > $NUM_HOSTS )); then
+    print_log "ERROR: Not enough hosts to run $NUM_AMRWIND_HOSTS AMR-WIND processes and $NUM_COLZA_HOSTS Colza processes"
+    exit -1
+fi
+if (( $NUM_AMRWIND_HOSTS < $NUM_COLZA_HOSTS )); then
+    print_log "ERROR: Cannot run with NUM_AMRWIND_HOSTS ($NUM_AMRWIND_HOSTS) < NUM_COLZA_HOSTS ($NUM_COLZA_HOSTS)"
+    exit -1
+fi
+
+print_log "Activating environment"
+source $HERE/settings.sh
+source $COLZA_EXP_SPACK_LOCATION/share/spack/setup-env.sh
+spack env activate $COLZA_EXP_SPACK_ENV
+
+PDOMAIN="colzamdorier"
+print_log "Setting up protection domain"
+apstat -P | grep ${PDOMAIN} || apmgr pdomain -c -u ${PDOMAIN}
+
+PROTOCOL=ofi+gni
+
+BEDROCK_CONFIG=$HERE/config/config.json
+BEDROCK_OUT="bedrock.out"
+BEDROCK_ERR="bedrock.err"
+BEDROCK_SSG_FILE=$(cat $BEDROCK_CONFIG | jq -r ".ssg|.[0]|.group_file")
+
+function ensure_bedrock_is_alive() {
+    $(kill -0 $BEDROCK_PID)
+    BEDROCK_IS_DEAD=$?
+    if [ $BEDROCK_IS_DEAD -ne 0 ];
+    then
+        print_log "Bedrock died, please see logs ($BEDROCK_OUT and $BEDROCK_ERR) for information"
+        exit -1
+    fi
+}
+
+if [ -z "$BEDROCK_SSG_FILE" ]
+then
+  print_log "ERROR: No SSG group file found in configuration"
+  exit -1
+fi
+
+print_log "Generating UUID for experiment"
+exp_id=$(uuidgen)
+exp_id=${exp_id:0:8}
+exp_date=$(date +"%Y-%m-%d-%H-%M")
+
+exp_dir="exp-$exp_date-$exp_id"
+print_log "Creating experiment's directory $exp_dir"
+mkdir $exp_dir
+cd $exp_dir
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HERE/sw/colza-ascent-pipeline/lib
+
+print_log "Starting Bedrock daemon"
+MPI_WRAPPERS=`spack location -i mochi-mona`/lib/libmona-mpi-wrappers.so
+aprun -cc none -n $NUM_COLZA_HOSTS -N 1 -e LD_PRELOAD=$MPI_WRAPPERS -p ${PDOMAIN} \
+    bedrock $PROTOCOL -c $BEDROCK_CONFIG -v trace 1> $BEDROCK_OUT 2> $BEDROCK_ERR &
+BEDROCK_PID=$!
+
+print_log "Waiting for SSG file to become available"
+while [ ! -f $BEDROCK_SSG_FILE ]
+do
+    ensure_bedrock_is_alive
+    sleep 1
+done
+sleep 1
+
+ensure_bedrock_is_alive
+print_log "Servers are ready"
+
+print_log "Starting AMR-WIND"
+AMR_WIND=$COLZA_EXP_PREFIX_PATH/amr-wind/bin/amr_wind
+AMR_WIND_INPUT=$HERE/input/laptop_scale.damBreak.i
+
+aprun -n $NUM_AMRWIND_HOSTS -N 1 -cc none -p $PDOMAIN $AMR_WIND $AMR_WIND_INPUT
+
+print_log "Shutting down servers"
+aprun -n 1 -N 1 -cc none -p $PDOMAIN bedrock-shutdown $PROTOCOL -s $BEDROCK_SSG_FILE
+
+print_log "Destroying protection domain"
+apmgr pdomain -r -u ${PDOMAIN}
+
+print_log "Terminating"
